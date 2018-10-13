477390da067326b90dd71e769c91e94d645b608a
Pabos95 <pabloagc95@gmail.com>
2018-10-13 14:44:18 -0600

    Metodos de nachostabla.cc creados

--- code/userprog/nachostabla.cc
+++ code/userprog/nachostabla.cc
@@ -1,1 +1,35 @@
- 
+#include nachostabla.h
+using namespace std;
+NachosOpenFilesTable::NachosOpenFilesTable(){ //el constructor inicializa un bitMap con n items bits
+    usage = 0;
+    openFilesMap = new Bitmap(256);
+    openFiles = new int[256];
+}
+NachosOpenFilesTable::~NachosOpenFilesTable(){
+    delete openFilesMap;
+    delete openFiles;
+}
+int NachosOpenFilesTable::Open(int UnixHandle){
+    int resultado = openFilesMap->Find();
+    if(resultado != -1){
+        openFiles[resultado] = UnixHandle;
+    }
+    return resultado;
+}
+int NachosOpenFilesTable::Close(int NachosHandle){
+    if(openFilesMap->Test(NachosHandle){
+        openFilesMap->Clear(NachosHandle);
+        openFiles[NachosHandle] = 0;
+        return 1;
+    }
+    return -1;
+}
+bool isOpened(int NachosHandle){
+    return openFilesMap->Test(NachosHandle);
+}
+void NachosOpenFilesTable::addThread(){ //si hay un nuevo hilo se aumenta la variable de uso de la  tabla
+  usage++;
+}
+void NachosOpenFilesTable::delThread(){ //si hay un nuevo hilo se disiminuye en uno la variable de uso de la tabla
+ usage--;
+}
